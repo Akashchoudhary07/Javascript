9515d8f8c780b38b4176528fa44c1f4e7540e58f
Akashchoudhary07 <aakashchaudhary0183@gmail.com>
2024-08-25 21:33:05 +0530

    javascript

---
+++ Basic Javascript/practice data/02Ext.js
@@ -0,0 +1,15 @@
+x= 12;
+
+document.write('dsfd 76567 ^&% <u>Underline</u>');// BODY 
+console.log("This is in CONSOLE");// CONSOLE
+
+ y =  document.getElementById('para')
+console.log(y);//h1 tag
+y.innerHTML = 'HEllo <u>H<i>1</i></u>'
+
+//   y.innerText ="hello <u>Fr<i>a</i>nk</u>..1"
+
+//   varname = tag 
+
+//   var.attr 
+//   var.method
